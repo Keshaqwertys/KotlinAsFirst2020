4a40c242cf7e11e8876e0eb8ea0ff41c80542cc0
leson 2, task 1 <uhovad03@gmail.com>
2022-03-05 02:03:11 +0300

    les 11

--- src/lesson11/task1/DimensionalValue.kt
+++ src/lesson11/task1/DimensionalValue.kt
@@ -2,6 +2,8 @@
 
 package lesson11.task1
 
+import ru.spbstu.kotlin.typeclass.classes.defaultValue
+
 /**
  * Класс "Величина с размерностью".
  *
@@ -16,27 +18,56 @@ package lesson11.task1
  * - либо соответствовать одной из приставок, к которой приписана сама размерность (Km, Kg, mm, mg)
  * - во всех остальных случаях следует бросить IllegalArgumentException
  */
+fun Reg(s: String): Pair<Double, String> {
+    if (!Regex("""\d+ [a-zA-Z]+""").matches(s)) throw IllegalArgumentException()
+    return (Pair(Regex("""\d+""").find(s)!!.value.toDouble(), Regex("""[a-zA-Z]+""").find(s)!!.value))
+}
+
 class DimensionalValue(value: Double, dimension: String) : Comparable<DimensionalValue> {
     /**
      * Величина с БАЗОВОЙ размерностью (например для 1.0Kg следует вернуть результат в граммах -- 1000.0)
      */
-    val value: Double get() = TODO()
+
+    private fun check(value: Double, dimension: String): Pair<Double, Dimension> {
+        val SetDimension = Dimension.values().toSet()
+        val MapDimension = mutableMapOf<String, Dimension>()
+        for (Dimension in SetDimension)
+            MapDimension[Dimension.abbreviation] = Dimension
+
+        val SetDimensionPrefix = DimensionPrefix.values().toSet()
+        val MapDimensionPrefix = mutableMapOf<String, Double>()
+        for (Prefix in SetDimensionPrefix)
+            MapDimensionPrefix[Prefix.abbreviation] = Prefix.multiplier
+
+        return if (dimension in MapDimension)
+            Pair(value, MapDimension[dimension]!!)
+        else if (dimension[0].toString() in MapDimensionPrefix && dimension.substring(1) in MapDimension)
+            Pair(value * MapDimensionPrefix[dimension[0].toString()]!!, MapDimension[dimension.substring(1)]!!)
+        else throw IllegalArgumentException()
+    }
+
+    val value = check(value, dimension).first
+
 
     /**
      * БАЗОВАЯ размерность (опять-таки для 1.0Kg следует вернуть GRAM)
      */
-    val dimension: Dimension get() = TODO()
+    val dimension = check(value, dimension).second
 
     /**
      * Конструктор из строки. Формат строки: значение пробел размерность (1 Kg, 3 mm, 100 g и так далее).
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(Reg(s).first, Reg(s).second)
 
     /**
      * Сложение с другой величиной. Если базовая размерность разная, бросить IllegalArgumentException
      * (нельзя складывать метры и килограммы)
      */
-    operator fun plus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun plus(other: DimensionalValue): DimensionalValue {
+        if (this.dimension == other.dimension) {
+            return DimensionalValue(this.value + other.value, this.dimension.abbreviation)
+        } else throw IllegalArgumentException()
+    }
 
     /**
      * Смена знака величины
@@ -46,7 +77,11 @@ class DimensionalValue(value: Double, dimension: String) : Comparable<Dimensiona
     /**
      * Вычитание другой величины. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    operator fun minus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun minus(other: DimensionalValue): DimensionalValue {
+        if (this.dimension == other.dimension) {
+            return DimensionalValue(this.value - other.value, this.dimension.abbreviation)
+        } else throw IllegalArgumentException()
+    }
 
     /**
      * Умножение на число
